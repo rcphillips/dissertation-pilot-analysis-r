29e1cfab206f54f11ac4976398804a8179091f68
Ryan Phillips <rcphillips88@gmail.com>
2015-05-12 15:17:32 -0700

    Got the spaghetti plot working! The colors don't look great, but it's pretty, clean code that gets the job done! and is well commented!

--- IndivSubjectAnalysis.R
+++ IndivSubjectAnalysis.R
@@ -6,18 +6,25 @@
 #increasingly cumbersome script, I'm just going to bring in the pieces from the
 #other script (PSS_to_task_plotter (merged).R) which are important for the output, and
 #keep it in the same working directory.
-###
+###pseudocode:
 #for a single subject:
 #Read csv
 #plot RT on x axis
 #plot count on y axis
-#join with SRP info
-#plot divided by sig
+#add grouping variable
+#
 #repeat for all subjects on the same plot
-###
-setwd("~/Box Sync/SRP_AXCPT_pilot_data")
-#subjno goes here:
-i = 8
+###housekeeping- setting working directory, initializing variables
+#setwd:
+#for IRC comp
+setwd("C:/Users/rphillips/Box Sync/SRP_AXCPT_pilot_data")
+#for CNS comp
+#setwd("~/Box Sync/SRP_AXCPT_pilot_data")
+#Initializing variables
+allsubjdata<-data.frame(NULL)
+###actual code begins
+#extract the data for all subjects
+for  (i in c(6,7,9,10,11,13,15,16,17,18,19,20,22)){
 #Read csv
 subjpss_name<- paste("subj", i, sep="","_pss.csv")
 subjpss<-read.csv(subjpss_name,stringsAsFactors=FALSE)
@@ -27,9 +34,12 @@ subjdata<-read.csv(subjdata_name,stringsAsFactors=FALSE)
 #Remove inaccurate trials from the subjdata. This part of the task is just concerned with
 #correct trials/partial lapses. Incorrect trials are dealt with elsewhere.
 subjdata<-subset(subjdata, subjdata$Cue.ACC=="1" & subjdata$Probe.ACC=="1")
+#bring all subjects into same data frame
+subjdata[length(subjdata)+1]<-i # give a group variable
+allsubjdata<-rbind(allsubjdata,subjdata) #join with data from previous loops
+}#end group processing
 #plot RT on x axis
-hist(subjdata$Probe.RT, breaks = 100, ylim=c(0,5))
 #plot count on y axis
-#join with SRP info
+hist(subjdata$Probe.RT, breaks = 100, ylim=c(0,5))
 #plot divided by sig
-#repeat for all subjects on the same plot
+
