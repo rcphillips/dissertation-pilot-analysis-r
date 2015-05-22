134591f1cb8dd11e70ef70144a9951020ef604b9
Ryan Phillips <rcphillips88@gmail.com>
2015-05-22 13:57:49 -0700

    Checked the timing in the optseq files itself, everything checks out, is exactly the same length

--- QA_on_Pilots.R
+++ QA_on_Pilots.R
@@ -7,9 +7,9 @@
 setwd("C:/Users/rphillips/Box Sync/SRP_AXCPT_pilot_data")
 #from home comp:
 #setwd("C:/Users/ryphil/Box Sync/SRP_AXCPT_pilot_data")
-last_trial_in_block<-matrix(nrow=5,ncol=1)
-final_cues_in_min_minus_sum<-matrix(nrow=5,ncol=1)
-allsubj_final_cues_in_min_minus_sum<-data.frame(blankcol=c(1:5))
+last_trial_in_block<-matrix(nrow=6,ncol=1)
+final_cues_in_min_minus_sum<-matrix(nrow=6,ncol=1)
+allsubj_final_cues_in_min_minus_sum<-data.frame(blankcol=c(1:6))
 ###Pseudocode:
 #load csv
 #Find the column that contains time
@@ -22,13 +22,13 @@ for  (i in c(6,7,9,10,11,13,15,16,17,18,19,20,22,23,24,25,26,27,28,29,30,31,32))
 subjdata_name<- paste("subj", i, sep="","_task.csv")
 subjdata<-read.csv(subjdata_name,stringsAsFactors=FALSE)
 #find the final item in each block
-for (j in 1:5){
+for (j in 1:6){
 last_trial_in_block[j]<-as.numeric(which(subjdata$BlockNum==j)[length(which(subjdata$BlockNum==j))])
 }
 
 #Find the column that contains time
 final_cues_in_ms<-subjdata$Cue.OnsetTime[last_trial_in_block]
-final_cues_in_s<-final_cues_in_ms/1000
+final_cues_in_s<-as.numeric(final_cues_in_ms)/1000
 final_cues_in_min<-final_cues_in_s/60
 final_cues_in_min#this gives us the endpoint of each block, but we need to subtract
                  #the previous blocks, so we can see when the block begins
@@ -49,3 +49,24 @@ table(subjdata$Subject, subjdata$BlockNum)
 #group it by block
 
 #subtract beginning time from end time
+#####
+#going directly to the optseq files
+?read.csv()
+file1<-read.csv("file1.csv", col.names = c("Type","ISI","ITI"), header = FALSE)
+file2<-read.csv("file2.csv", col.names = c("Type","ISI","ITI"), header = FALSE)
+file3<-read.csv("file3.csv", col.names = c("Type","ISI","ITI"), header = FALSE)
+file4<-read.csv("file4.csv", col.names = c("Type","ISI","ITI"), header = FALSE)
+file5<-read.csv("file5.csv", col.names = c("Type","ISI","ITI"), header = FALSE)
+tail(file1)
+tail(file2)
+tail(file3)
+tail(file4)
+tail(file5)
+sum(file3$ITI)
+sum(file4$ITI)
+sum(file5$ITI)
+
+#now checking the eprime output
+subjdata<-read.csv("testof150520timing.csv", stringsAsFactors=FALSE)
+colnames(subjdata)<-subjdata[1,]
+  
